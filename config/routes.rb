c41132d931dee3322ed24a70a6397c6cc003cd4a
Adam McCrea <adam@adamlogic.com>
2020-11-26 13:39:18 -0500

    Add job dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  if defined?(Delayed)
+    mount Delayed::Web::Engine, at: '/jobs'
+  end
+
+  if defined?(Sidekiq)
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/jobs'
+  end
+
+  if defined?(Resque)
+    require 'resque/server'
+    mount Resque::Server.new, :at => '/jobs'
+  end
+
+  if defined?(Que)
+    require "que/web"
+    mount Que::Web => "/jobs"
+  end
+
   root to: 'home#show'
 end
